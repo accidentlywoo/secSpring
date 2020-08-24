f6c28fd4d3b8ea4cfcffd2a2f67a3b3778dfc4ff
accidentlywoo <accidentlywoo@gmail.com>
2020-08-24 10:52:22 +0900

    Collection을 이용한 Multi Error 발생, Assuming 사용해보기, @Test @DisplayName을 작성해서 테스트의 의미있는 이름을 작성하지!

--- di/src/test/JUnitTest.java
+++ di/src/test/JUnitTest.java
@@ -1,8 +1,11 @@
 package test;
 
+import static org.junit.Assume.assumeThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.my.vo.Customer;
@@ -15,6 +18,7 @@ class JUnitTest {
 	}
 
 	@Test
+	@DisplayName(value = "롬복문 라이브러리 테스트 : 단정문(assert)")
 	void testLombokPersonVO() {
 //		fail("Not yet implemented"); 무조건 테스트 실패
 		Person p1, p2;
@@ -24,6 +28,7 @@ class JUnitTest {
 	}
 	
 	@Test
+	@DisplayName(value = "롬복문 라이브러리 테스트2 : 단정문(assert)")
 	void testLombokCustomerVO() {
 		Customer c1, c2;
 		c1 = new Customer("id1", "p1");
@@ -36,8 +41,20 @@ class JUnitTest {
 		assertNotEquals(c1, c2);// equals() 오버라이딩
 		
 		// 테스트 실패시 1번째 실패에서 테스트 종료된다. -> 여러개를 테스트 하고 싶을 땐?
-		assertNotNull(c1.getName());
-		assertNotNull(c1.getAddr());
+//		assertNotNull(c1.getName());
+//		assertNotNull(c1.getAddr());
+		
+		// Collection을 이용해서 Multiple Error를 방생시키자.
+		assertAll(
+				() -> assertNotNull(c1.getName()),
+				() -> assertNotNull(c1.getAddr())
+		);
 	}
-
+	
+	@Test
+	@DisplayName(value = "가정문(assuming)")
+	void testAssume() {
+		int num = 10;
+		assumingThat(num % 2 == 0, () -> assertEquals(num, 10));
+	}
 }
