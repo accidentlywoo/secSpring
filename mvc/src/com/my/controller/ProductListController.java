a2b0a9cc9a47e0cace815c74e188ef5ec44407ca
accidentlywoo <accidentlywoo@gmail.com>
2020-08-27 10:27:22 +0900

    index화면 구동되세 바꾸기

---
+++ mvc/src/com/my/controller/ProductListController.java
@@ -0,0 +1,36 @@
+package com.my.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.my.exception.FindException;
+import com.my.service.ProductService;
+import com.my.vo.Product;
+
+@Controller
+public class ProductListController {
+	@Autowired
+	private ProductService productSerive;
+	
+	@GetMapping(value = "/productList")
+	public ModelAndView productList(){
+		ModelAndView mnv = new ModelAndView();
+		List<Product> list;
+		try {
+			list = productSerive.findAll();
+			mnv.addObject("list", list);
+			mnv.setViewName("/jsp/productList");
+		} catch (FindException e) {
+			mnv.addObject("errorMsg", e.getMessage());
+			mnv.setViewName("/fail");
+		}
+		return mnv;
+	}
+}
