d13bbcc548cc5f7b297e37f8f0c97c7b4ae26de1
accidentlywoo <accidentlywoo@gmail.com>
2020-08-31 11:15:42 +0900

    REST 형식의 요청을 활용하는 방법 @PathVariable

--- mvc/src/com/my/controller/RestTestController.java
+++ mvc/src/com/my/controller/RestTestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -37,4 +38,10 @@ public class RestTestController {
 		return list;
 	}
 	
+	// http://localhost/mvc/v.do?no=1 (X)
+	// http://localhost/mvc/s.do/1 (0)
+	@GetMapping(value = "/v.do/{no}") 
+	public String e(@PathVariable(value = "no") int no) {
+		return "@PathVariacble : " + no;
+	}
 }
