21659f486ecbf8df09a9ab2e2f4f5e36e90155d2
accidentlywoo <accidentlywoo@gmail.com>
2020-09-01 15:21:30 +0900

    주문 DAO Transactional Handling : 사용자 정의 Exception으로 Rollback Flow Controll

--- mvc/src/com/my/dao/OrderDAO.java
+++ mvc/src/com/my/dao/OrderDAO.java
@@ -8,39 +8,52 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.my.exception.AddException;
 import com.my.exception.FindException;
 import com.my.vo.OrderInfo;
 import com.my.vo.OrderLine;
 
+import lombok.extern.log4j.Log4j;
+
 @Repository
+@Log4j
 public class OrderDAO {
 	@Autowired
 	private SqlSessionFactory sessionFactory;
 
+	@Transactional(rollbackFor = AddException.class)
 	public void insert(OrderInfo info) throws AddException { // Transaction
-		SqlSession session = null;
+		log.info("");
 		try {
-			session = sessionFactory.openSession(ExecutorType.BATCH, false);
-			insertInfo(session, info);
-			insertLines(session, info.getLines());
-			session.commit();
-		} catch (DataAccessException e) { // SQLSyntexError로 잡지 않는다.
-			session.rollback();
+			insertInfo(info);
+			insertLines(info.getLines());
+		} catch (Exception e) { // SQLSyntexError로 잡지 않는다.
 			throw new AddException(e.getMessage());
 		} finally {
-			session.close();
 		}
 	}
 
-	private void insertInfo(SqlSession session, OrderInfo info) throws AddException {
-		int result = session.insert("OrderMapper.insertInfo", info.getOrder_c().getId());
+	private void insertInfo(OrderInfo info) throws AddException {
+		SqlSession session = sessionFactory.openSession();
+		try {
+			session.insert("OrderMapper.insertInfo", info.getOrder_c().getId());
+		} catch (Exception e) {
+			throw new AddException(e.getMessage());
+		}
 	}
 
-	private void insertLines(SqlSession session, List<OrderLine> lines) throws AddException {
-		for (OrderLine item : lines) {
-			session.insert("OrderMapper.insertLines", item);
+	private void insertLines(List<OrderLine> lines) throws AddException {
+		SqlSession session = sessionFactory.openSession(ExecutorType.BATCH);
+		try {
+			for (OrderLine line : lines) {
+				session.insert("OrderMapper.insertLines", line);
+			}
+			// MyBatis는 Batch 기능이없다. DB에서 Batch 작업을 도와주는 기능을 주현해야 한다.
+			session.flushStatements();
+		} catch (Exception e) {
+			throw new AddException(e.getMessage());
 		}
 	}
 
